c6a3aa0166a404451b7507b30f82715d3886072a
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-23 01:26:55 +0300

    added СharacterDTO

--- xcode_ios_home/xcode_ios_home/ViewController.swift
+++ xcode_ios_home/xcode_ios_home/ViewController.swift
@@ -1,19 +1,41 @@
 //
 //  ViewController.swift
-//  xcode_ios_home
+//  xcode_ios_class
 //
 //  Created by Rezo on 09.10.2023.
 //
-
+// hello!
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemGreen
+        let url: URL = URL(string: "https://gravity-falls-api.vercel.app/api/characters")!
+        URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            guard
+                let data = data,
+                error == nil
+            else {
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            //Обработка данных
+            let model = try! decoder.decode([СharacterDTO].self, from: data)
+            print(model)
+        }).resume()
+        
+        
     }
-
-
 }
 
+
+struct СharacterDTO: Decodable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let imageUrl: URL
+    
+}
